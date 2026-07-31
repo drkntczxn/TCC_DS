2cea1f61c0ae331f3ece959e6066d059f753df3e
Nícolas <ntr7nicolas@gmail.com>
2026-07-31 13:12:13 -0300

    index altered

--- connection.php
+++ connection.php
@@ -1,5 +1,5 @@
 <?php
-$con = mysqli_connect("localhost", "root", "102780", "carwash", "3306");
+$con = mysqli_connect("localhost", "root", "", "carwash", "3308");
 
 if(!$con){
     die("Connection error: ".mysqli_connect_error());
--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
-    <title>Car Wash</title>
+    <title>Ótica Fany</title>
 </head>
 
 <body>
